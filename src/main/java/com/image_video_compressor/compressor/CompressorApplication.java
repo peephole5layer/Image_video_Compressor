0c586f810ff9fc589e368cd30d2c77d6ab0564f0
peephole5layer <divyanshu002mishra@gmail.com>
2025-04-03 17:43:35 +0530

    Initial commit

---
+++ src/main/java/com/image_video_compressor/compressor/CompressorApplication.java
@@ -0,0 +1,13 @@
+package com.image_video_compressor.compressor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompressorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompressorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/image_video_compressor/compressor/CompressorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.image_video_compressor.compressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompressorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
